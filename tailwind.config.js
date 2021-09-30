b982c1d961ac64aae287c2069e221a30fec5dcd2
Аня <suharevskaya@firecode.ru>
2021-09-30 11:35:50 +0300

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      // Configure your color palette here
+      textColor: {
+        DEFAULT: '#3E3E3C',
+      },
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
