fea7da7c0d205bbabb9b41a445da9fb3a195b4b8
Аня <suharevskaya@firecode.ru>
2021-09-30 15:49:21 +0300

    start markers-list

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      // Configure your color palette here
-      textColor: {
-        DEFAULT: '#3E3E3C',
-      },
-    }
+    extend: {
+      colors: {
+        'textColor':'#3E3E3C',
+      }
+    },
   },
   variants: {
     extend: {},
